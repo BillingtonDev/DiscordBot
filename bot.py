864ae1d17a730b1486e3bd312b0293549a52d5ad
BillingtonDev <119828882+BillingtonDev@users.noreply.github.com>
2025-02-21 12:17:25 -0500

    MB - comments

--- bot.py
+++ bot.py
@@ -17,10 +17,9 @@ GUILD = os.getenv('DISCORD_GUILD')
 intents = discord.Intents.default()  
 intents.message_content = True  
 
-
-#bot = discord.bot(intents=intents)
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# Succesful connection message to terminal.
 @bot.event
 async def on_ready():
     guild = discord.utils.get(bot.guilds, name=GUILD)
@@ -28,14 +27,16 @@ async def on_ready():
         f'{bot.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
-    
+
+# Greet new chat members
 @bot.event
 async def on_member_join(member):
     await member.create.dm()
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to my Discord server!'
     )
-    
+
+# Exception handeling on events    
 @bot.event
 async def on_error(event, *args, **kwargs):
     with open('err.log', 'a') as f:
@@ -44,6 +45,7 @@ async def on_error(event, *args, **kwargs):
         else:
             raise
 
+# Random Winston Churchill quote generator.
 @bot.command(name='wc', help='Responds with a random quote from Winston Churchill.')
 async def winston(ctx):
     wc_quotes = [
@@ -55,6 +57,7 @@ async def winston(ctx):
     response = random.choice(wc_quotes)
     await ctx.send(response)
 
+# Dice simulator
 @bot.command(name='roll_dice', help='Simulate rolling dice. Enter as !ctx num_dice num_sides.')
 async def roll(ctx, num_dice: int, num_sides: int):
     dice = [
@@ -63,7 +66,7 @@ async def roll(ctx, num_dice: int, num_sides: int):
     ]
     await ctx.send(', '.join(dice))
         
-        
+# Create new chats    
 @bot.command(name='create-channel')
 @commands.has_role('admin')
 async def create_channel(ctx, channel_name):
@@ -72,7 +75,8 @@ async def create_channel(ctx, channel_name):
     if not existing_channel:
         print(f'Creating a new channel: {channel_name}')
         await guild.create_text_channel(channel_name)   
-        
+      
+# Handle admin exceptions on !create-channel  
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
